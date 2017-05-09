98601a0cb90f14c3bc5d6ca81fe05d63db65db5a
Cleo <docterwater@gmail.com>
2017-05-09 15:54:49 +0800

    複製admin.users.create 到 admin.users.edit

---
+++ resources/views/admin/users/edit.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1>Create Users</h1>
+
+    {!! Form::open(['method'=>'POST','action'=>'AdminUserController@store','files'=>true]) !!}
+
+    <div class="form-group">
+
+        {{--+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++--}}
+        {{--{!! Form::text('id','value' or null ,['attribute'=>'class or placeholder...']) !!}--}}
+        {{--+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++--}}
+
+
+        {!! Form::label('name','Name') !!}
+        {!! Form::text('name',null,['class'=>'form-control']) !!}
+
+    </div>
+
+    <div class="form-group">
+
+        {!! Form::label('email','Email') !!}
+        {!! Form::email('email',null,['class'=>'form-control']) !!}
+
+    </div>
+
+    <div class="form-group">
+
+        {!! Form::label('role_id','Role') !!}
+        {!! Form::select('role_id',[''=>'Choose Options'] + $roles ,null,['class'=>'form-control']) !!}
+
+    </div>
+
+    <div class="form-group">
+
+        {!! Form::label('is_active','Status') !!}
+        {!! Form::select('is_active',['0'=>'Not Active','1'=>'Active'],'1',['class'=>'form-control']) !!}
+
+    </div>
+
+    <div class="form-group">
+
+        {!! Form::label('photo_id','File') !!}
+        {!! Form::file('photo_id',null,['class'=>'form-control']) !!}
+
+    </div>
+
+    <div class="form-group">
+
+        {!! Form::label('password','Password') !!}
+        {!! Form::password('password',null,['class'=>'form-control']) !!}
+
+    </div>
+
+
+    <div class="form-group">
+        {!! Form::submit('create User',['class'=>'btn btn-primary']) !!}
+    </div>
+
+    {!! Form::close() !!}
+
+    @include('includes.form_error')
+
+
+
+
+@stop
