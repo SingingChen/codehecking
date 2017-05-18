e598bdf9d3c6b7e11a9eb5dfdf6b625ebb6db59c
Cleo <docterwater@gmail.com>
2017-05-18 10:05:56 +0800

    建立postCreate.view

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -6,4 +6,43 @@
     <h1>Create Post</h1>
 
 
+     {!! Form::open(['method'=>'POST','action'=>'AdminPostController@store' ,'files'=>true]) !!}
+
+         <div class="form-group">
+
+         {{--+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++--}}
+             {{--{!! Form::text('id','value' or null ,['attribute'=>'class or placeholder...']) !!}--}}
+          {{--+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++--}}
+
+
+             {!! Form::label('title','Title') !!}
+             {!! Form::text('title',null,['class'=>'form-control']) !!}
+         </div>
+
+         <div class="form-group">
+             {!! Form::label('category_id','Category') !!}
+             {!! Form::select('category_id',array(''=>'Options'),null,['class'=>'form-control']) !!}
+         </div>
+
+         <div class="form-group">
+             {!! Form::label('photo_id','Photo') !!}
+             {!! Form::file('photo_id',null,['class'=>'form-control']) !!}
+         </div>
+
+         <div class="form-group">
+             {!! Form::label('body','Description') !!}
+             {!! Form::textarea('body',null,['placeholder'=>'enter content','cols'=>100]) !!}
+         </div>
+
+         <div class="form-group">
+            {!! Form::submit('create post',['class'=>'btn btn-primary']) !!}
+         </div>
+
+         {!! Form::close() !!}
+
+
+        @include('includes.form_error')
+
+
+
 @endsection
