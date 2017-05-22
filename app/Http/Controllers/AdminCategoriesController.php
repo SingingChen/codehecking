fbcfb7f30657bd57c739610c48b9a92a36ce88ac
Cleo <docterwater@gmail.com>
2017-05-22 16:17:34 +0800

    完成更新 刪除categories

--- app/Http/Controllers/AdminCategoriesController.php
+++ app/Http/Controllers/AdminCategoriesController.php
@@ -15,7 +15,7 @@ class AdminCategoriesController extends Controller
     public function index()
     {
         $categories = Category::all();
-        return view('admin/categories/index',compact('categories'));
+        return view('admin.categories.index',compact('categories'));
     }
 
     /**
@@ -60,7 +60,9 @@ class AdminCategoriesController extends Controller
      */
     public function edit($id)
     {
-        //
+        $category = Category::findOrFail($id);
+
+        return view('admin.categories.edit',compact('category'));
     }
 
     /**
@@ -72,7 +74,9 @@ class AdminCategoriesController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        Category::findOrFail($id)->update($request->all());
+
+        return redirect('admin/categories');
     }
 
     /**
@@ -83,6 +87,8 @@ class AdminCategoriesController extends Controller
      */
     public function destroy($id)
     {
-        //
+        Category::findOrFail($id)->delete();
+
+        return redirect('admin/categories');
     }
 }
