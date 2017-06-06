6a56d383cab2c481a05815b8132a9eb938482259
Cleo <docterwater@gmail.com>
2017-06-06 15:06:19 +0800

    可讓使用者編輯文字 :1.參考 https://www.tinymce.com/  2. 新增一個頁面 includes/tinyeditor 3.放入<script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>,<script>tinymce.init({ selector:'textarea' });</script>可基本運作 4.參考安裝：http://image.intervention.io/getting_started/installation   可resize 圖片 5. 參考安裝：https://github.com/UniSharp/laravel-filemanager（須先安裝第4點）

--- config/app.php
+++ config/app.php
@@ -180,6 +180,12 @@ return [
 
 
         Cviebrock\EloquentSluggable\ServiceProvider::class,
+
+//        編輯文字 圖片
+        Intervention\Image\ImageServiceProvider::class,
+
+         Unisharp\Laravelfilemanager\LaravelFilemanagerServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
     ],
 
     /*
@@ -230,6 +236,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        'Image' => Intervention\Image\Facades\Image::class
 
 
     ],
