9fa71203deb77740a00721a1bd70543538bb499e
Cleo <docterwater@gmail.com>
2017-06-06 16:32:39 +0800

    利用disques 建立討論 並在網頁進行分析。https://disqus.com/admin/?email_verified=1&utm_source=email_verified

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -30,126 +30,28 @@
     <hr>
 
 
-    @if(Session::has('comment_message'))
-        {{session('comment_message')}}
-
-
-        @endif
-
-    <!-- Blog Comments -->
-@if(Auth::check())
-    <!-- Comments Form -->
-    <div class="well">
-     {!! Form::open(['method'=>'POST','action'=>'PostCommentsController@store']) !!}
-
-         <div class="form-group">
-
-             <input type="hidden" name="post_id" value="{{$post->id}}">
-             <input type="hidden" name="email" value="{{$post->user->email}}">
-             <input type="hidden" name="author" value="{{$post->user->name}}">
-
-             {!! Form::label('body','Leave Comment') !!}
-             {!! Form::textarea('body',null,['class'=>'form-control','row'=>3]) !!}
-
-         </div>
-
-         <div class="form-group">
-            {!! Form::submit('Post Comment',['class'=>'btn btn-primary']) !!}
-         </div>
-
-         {!! Form::close() !!}
-
-    </div>
-@endif
-    <hr>
-
-    <!-- Posted Comments -->
-
-    <!-- Comment -->
-    @if(count($comments)>0)
-        @foreach($comments as $comment)
-    <div class="media">
-        <a class="pull-left" href="#">
-            <img height="64" class="media-object" src="{{$comment->photo}}" alt="">
-        </a>
-        <div class="media-body">
-            <h4 class="media-heading">{{$comment->author}}
-                <small>{{$comment->created_at->diffForHumans()}}</small>
-            </h4> {{$comment->body}}
-
-            <!-- Nested Comment -->
-
-        @if(count($comment->replies)>0)
-
-            @foreach($comment->replies as $reply)
-
-                @if($reply->is_active == 1)
-                    <div class="media nested-comment">
-                        <a class="pull-left" href="#">
-                            <img height="64" class="media-object" src="{{Auth::user()->gravatar}}" alt="">
-                        </a>
-                        <div class="media-body">
-                            <h4 class="media-heading">{{$reply->author}}
-                                <small>{{$reply->created_at->diffForHumans()}}</small>
-                            </h4>
-                            {{$reply->body}}
-                        </div>
-                    </div>
-                @endif
-            @endforeach
-
-
-                @if(Session::has('reply_message'))
-
-                    {{session('reply_message')}}
-                    @endif
-            <div class="comment-reply-container">
-
-                <button class="toggle_reply btn btn-primary pull-right">Reply</button>
-
-                <div class="comment_reply col-sm-7">
-                    {!! Form::open(['method'=>'POST','action'=>'CommentRepliesController@createReply']) !!}
-
-                    <div class="form-group">
-
-                        <input type="hidden" name="comment_id" value="{{$comment->id}}">
-                        {!! Form::label('body','Body') !!}
-                        {!! Form::textarea('body',null,['class'=>'form-control','rows'=>1]) !!}
-
-                    </div>
-
-                    <div class="form-group">
-                        {!! Form::submit('Submit',['class'=>'btn btn-primary']) !!}
-                    </div>
-
-                    {!! Form::close() !!}
-                </div>
-
-            </div>
-        @endif
-        </div>
-        <!-- End Nested Comment -->
-
-    </div>
-        @endforeach
-
-    @endif
-
-    @stop
-
-@section('scripts')
-    <script
-            src="https://code.jquery.com/jquery-3.2.1.min.js"
-            integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
-            crossorigin="anonymous"></script>
+    <div id="disqus_thread"></div>
     <script>
 
-        $('.comment-reply-container .toggle_reply').click(function(){
-            $(this).next().slideToggle();
-
-        })
-
-
+        /**
+         *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+         *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+        /*
+         var disqus_config = function () {
+         this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+         this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+         };
+         */
+        (function() { // DON'T EDIT BELOW THIS LINE
+            var d = document, s = d.createElement('script');
+            s.src = 'https://codehecking.disqus.com/embed.js';
+            s.setAttribute('data-timestamp', +new Date());
+            (d.head || d.body).appendChild(s);
+        })();
     </script>
+    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
 
-    @stop
+
+    <script id="dsq-count-scr" src="//codehecking.disqus.com/count.js" async></script>
+
+@stop
