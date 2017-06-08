5e30125d10764fe137428231656b760b1000b442
Cleo <docterwater@gmail.com>
2017-06-08 13:56:23 +0800

    試試看git work

--- resources/views/admin/media/index.blade.php
+++ resources/views/admin/media/index.blade.php
@@ -5,37 +5,42 @@
     <h1>Medias</h1>
 
     @if($photos)
-     <table class="table">
-         <thead>
-           <tr>
-             <th>Id</th>
-             <th>Photo</th>
-             <th>Create Date</th>
-             <th>Delete</th>
 
-           </tr>
-         </thead>
-         <tbody>
-         @foreach($photos as $photo)
-           <tr>
-             <td>{{$photo->id}}</td>
-             <td><img height="50" src="{{$photo->file ? $photo->file:'No Image'}}" alt=""></td>
-             <td>{{$photo->created_at ? $photo->created_at->diffForHumans() : 'No Photo'}}</td>
-             <td>
-                  {!! Form::open(['method'=>'DELETE','action'=>['AdminMediasController@destroy',$photo->id]]) !!}
+        <form action="">
 
 
-                      <div class="form-group">
-                         {!! Form::submit('Delete',['class'=>'btn btn-danger']) !!}
-                      </div>
+             <table class="table">
+                 <thead>
+                   <tr>
+                     <th>Id</th>
+                     <th>Photo</th>
+                     <th>Create Date</th>
+                     <th>Delete</th>
 
-                      {!! Form::close() !!}
+                   </tr>
+                 </thead>
+                 <tbody>
+                 @foreach($photos as $photo)
+                   <tr>
+                     <td>{{$photo->id}}</td>
+                     <td><img height="50" src="{{$photo->file ? $photo->file:'No Image'}}" alt=""></td>
+                     <td>{{$photo->created_at ? $photo->created_at->diffForHumans() : 'No Photo'}}</td>
+                     <td>
+                          {!! Form::open(['method'=>'DELETE','action'=>['AdminMediasController@destroy',$photo->id]]) !!}
 
-             </td>
-           </tr>
-          @endforeach
-         </tbody>
-       </table>
+
+                              <div class="form-group">
+                                 {!! Form::submit('Delete',['class'=>'btn btn-danger']) !!}
+                              </div>
+
+                              {!! Form::close() !!}
+
+                     </td>
+                   </tr>
+                  @endforeach
+                 </tbody>
+               </table>
+        </form>
      @endif
 
 
